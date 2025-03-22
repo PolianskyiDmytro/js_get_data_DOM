e25bc89b9e8075dd3e599b87da73ea84692d42be
Polianskyi Dmytro <dimdemon1@gmail.com>
2025-03-22 13:29:55 +0200

    add more fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -13,31 +13,24 @@ const averagePopulation = Math.round(
   Number(totalPopulation) / population.length,
 ).toString();
 
-let totalResult = '';
-let averageResult = '';
+function numWithCommas(num) {
+  let j = 0;
+  let result = '';
 
-let j = 0;
+  for (let i = num.length - 1; i >= 0; i--) {
+    result = num[i] + result;
+    j++;
 
-for (let i = totalPopulation.length - 1; i >= 0; i--) {
-  totalResult = totalPopulation[i] + totalResult;
-  j++;
-
-  if (j % 3 === 0 && i !== 0) {
-    totalResult = ',' + totalResult;
+    if (j % 3 === 0 && i !== 0) {
+      result = ',' + result;
+    }
   }
+
+  return result;
 }
 
-j = 0;
+document.querySelector('span.total-population').textContent =
+  numWithCommas(totalPopulation);
 
-for (let i = averagePopulation.length - 1; i >= 0; i--) {
-  averageResult = averagePopulation[i] + averageResult;
-  j++;
-
-  if (j % 3 === 0 && i !== 0) {
-    averageResult = ',' + averageResult;
-  }
-}
-
-document.querySelector('span.total-population').textContent = totalResult;
-
-document.querySelector('span.average-population').textContent = averageResult;
+document.querySelector('span.average-population').textContent =
+  numWithCommas(averagePopulation);
